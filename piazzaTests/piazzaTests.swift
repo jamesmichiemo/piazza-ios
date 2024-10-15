d808ca0a80f9a58eabc77cdef53387cefc764fc4
James Michiemo <jmichiem@unca.edu>
2024-10-15 11:46:23 -0400

    Initial Commit

---
+++ piazzaTests/piazzaTests.swift
@@ -0,0 +1,17 @@
+//
+//  piazzaTests.swift
+//  piazzaTests
+//
+//  Created by James Michiemo on 10/15/24.
+//
+
+import Testing
+@testable import piazza
+
+struct piazzaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
